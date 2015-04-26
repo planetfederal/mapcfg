c3fd4db1d82556d8d95eefd814f6b5d0bc4c45ae
vmalaret <malaretv@gmail.com>
2015-04-26 11:33:03 -0400

    add test

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var prettyJs = require('pretty-js');
+const prettyJs = require('pretty-js');
 
 function identity(x) { return x; }
 
@@ -8,7 +8,7 @@ function map(o, func) {
 
 function objToString(obj, valTransform = _.identity) {
 
-  var str = map(obj, function(v, k) {
+  let str = map(obj, function(v, k) {
     let val = valTransform(v, k);
     return `${k}: ${val}`;
   }).join(',');
@@ -16,11 +16,18 @@ function objToString(obj, valTransform = _.identity) {
   return `{${str}}`;
 }
 
+/** Config Parser. Generates object to pass into ol.map */
 let parse = {};
 
 parse.createObj = function(type, v, k = '') {
-  var subtype = v.type || k;
-  var opts = v.opts || v;
+  let subtype = v.type || k;
+  let opts = v.opts || v;
+
+  // if ol present go ahead and do some basic validation
+  if (ol && (typeof ol[type] === undefined ||
+      (subtype && typeof ol[type][subtype] === undefined))) {
+    throw new Error(`ol.${type}${subtype} does not exist`);
+  }
 
   if (subtype !== '') {
     subtype = '.' + subtype;
@@ -29,7 +36,6 @@ parse.createObj = function(type, v, k = '') {
   if (opts) {
     opts = objToString(opts, (v, k) => {
       if (typeof v === 'string' && v.indexOf('new ol.') === 0) {
-        console.log()
         return v;
       } else {
         return JSON.stringify(v);
@@ -39,29 +45,28 @@ parse.createObj = function(type, v, k = '') {
   return `new ol.${type}${subtype}(${opts})`
 }
 
+parse.array = function(type, v, k) {
+  return '[' + map(v, (v, k) => parse[type](v,k)).join(',') + ']'
+};
+
 parse.view = parse.createObj.bind(this, 'View');
 parse.control = parse.createObj.bind(this, 'control');
 parse.source = parse.createObj.bind(this, 'source')
 
-parse.controls = function(v, k) {
-  return '[' + map(v, (v, k) => parse.control(v, k)).join(',') + ']';
-};
+parse.controls = parse.array.bind(this, 'control');
+parse.layers = parse.array.bind(this, 'layer');
+
 
 parse.layer = function(v, k) {
  v.opts.source = parse.source(v.opts.source);
  return parse.createObj('layer', v, k);
 };
 
-parse.layers = function(v, k) {
- return '[' + map(v, (v, k) => parse.layer(v)).join(',') + ']';
-};
-
 parse.map = function(obj) {
   return objToString(obj, (v, k) => parse[k] ? parse[k](v) : JSON.stringify(v));
 };
 
-
 module.exports = function(cfg) {
-  var str = parse.map(cfg);
-  return prettyJs(str, {indent: '  ',});
+  let str = parse.map(cfg);
+  return prettyJs('var map = new ol.Map({' + str + '})', {indent: '  ',});
 };
--- tests/index.js
+++ tests/index.js
@@ -1,15 +1,24 @@
 var test = require('tape')
 var fs = require('fs');
 var mapcfg = require('../');
+var ol = require('openlayers');
+window.ol = ol;
 
 test('basic', function(t) {
   var output = mapcfg(require('./basic.json'));
-  t.ok(true);
+  t.doesNotThrow(eval.bind(this, 'var x = ' + output));
   t.end();
 });
 
 test('wms', function(t) {
   var output = mapcfg(require('./wms.json'));
-  t.ok(true);
+  t.doesNotThrow(eval.bind(this, 'var x = ' + output));
   t.end();
 });
+
+test('error', function(t) {
+  var output = mapcfg(require('./error.json'));
+  t.throws(eval.bind(this, 'var x = ' + output));
+  t.end();
+});
+
